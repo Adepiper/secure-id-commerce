65c8c7a64015d7765df19e9d55fa285bcd681d73
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2023-09-13 12:42:39 +0100

    added secure id cookie

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Product } from '../models/product';
+import { products } from '../metadata/mockProductData';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private products = new BehaviorSubject<any[]>([]);
+  private products = new BehaviorSubject<Product[]>(products);
   products$ = this.products.asObservable();
 
-  private cartItems = new BehaviorSubject<any[]>([]);
+  private cartItems = new BehaviorSubject<Product[]>([]);
   cartItems$ = this.cartItems.asObservable();
 
   constructor() {}
 
-  addToCart(cart: any) {
+  addToCart(cart: Product) {
     const items = [...this.cartItems.value, cart];
     this.cartItems.next(items);
   }
 
-  removeFromCart(cart: any) {
+  removeFromCart(cart: Product) {
     const items = [...this.cartItems.value].filter(
-      (item) => item.id !== cart.id
+      (item) => item.productId !== cart.productId
     );
 
     this.cartItems.next(items);
