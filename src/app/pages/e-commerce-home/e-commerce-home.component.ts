5bafa4697bde1628da42c7cdf2aadcb615050d5c
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2023-09-13 12:57:13 +0100

    cart added

--- src/app/pages/e-commerce-home/e-commerce-home.component.ts
+++ src/app/pages/e-commerce-home/e-commerce-home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-e-commerce-home',
   templateUrl: './e-commerce-home.component.html',
-  styleUrls: ['./e-commerce-home.component.scss']
+  styleUrls: ['./e-commerce-home.component.scss'],
 })
-export class ECommerceHomeComponent {
+export class ECommerceHomeComponent implements OnInit {
+  constructor(private productService: ProductService) {}
+  cartItems$ = this.productService.cartItems$;
+  cartItems!: Product[];
 
+  ngOnInit(): void {
+    this.cartItems$.subscribe((data) => {
+      this.cartItems = data;
+    });
+  }
+
+  products$ = this.productService.products$;
+
+  addTocart(product: Product) {
+    this.itemInCart(product)
+      ? this.productService.removeFromCart(product)
+      : this.productService.addToCart(product);
+  }
+
+  itemInCart(product: Product) {
+    return this.cartItems.includes(product);
+  }
 }
